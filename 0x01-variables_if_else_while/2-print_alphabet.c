b459928ce24fd0465aa2bb0f865d3eb6ab37a179
odogwukelly <odogwukellyice@gmail.com>
2022-12-12 23:18:18 -0800

    a code that prints lowercase of char, using putchar alone

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a c code that prints alphabet in lower case
+ * by odogwu kelly
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char lowerCase;
+
+	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+	{
+		putchar(lowerCase);
+	}
+	putchar("\n");
+	return (0);
+}
