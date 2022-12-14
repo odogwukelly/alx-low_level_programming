1db2d168458d37cd33324e5db092e2025c95751c
odogwukelly <odogwukellyice@gmail.com>
2022-12-13 23:23:37 -0800

    a function that computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute number
+ * by odogwu kelly
+ * @x: parameter
+ * Return: absolute value of a number
+ */
+
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		x = -x;
+	}
+	return (x);
+}
