be8661b18242879380874ff7a2eaf7239c416353
odogwukelly <odogwukellyice@gmail.com>
2022-12-13 20:28:15 -0800

    a function that checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - check for alphabet letters
  * by odogwu kelly
  * @c: character argument
- * Return: returns 1 or 0 depending on the condition
+ * Return: returns 0 or 1 depending on the condition
  */
 
 int _isalpha(int c)
