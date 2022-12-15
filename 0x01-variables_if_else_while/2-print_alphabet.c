59c3ced61685b498c061bd111905f9d912187bb3
odogwukelly <odogwukellyice@gmail.com>
2022-12-15 00:36:12 -0800

    a program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,6 +2,7 @@
 /**
  * main - a c code that prints alphabet in lower case
  * by odogwu kelly
+ *
  * Return: Always 0 (success)
 */
 
@@ -10,9 +11,9 @@ int main(void)
 	char lowerCase;
 
 	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
-	{
 		putchar(lowerCase);
-	}
+
 	putchar("\n");
+
 	return (0);
 }
