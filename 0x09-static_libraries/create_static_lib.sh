25c16970d07911cf9abe24a8a4dbdb3c7effd770
odogwukelly <odogwukellyice@gmail.com>
2023-03-24 11:28:13 -0700

    hot fix

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-gcc -wall -pedantic -werror -wextra -c *.c
-ar rc liball.a *.o
+
+# Compile all the .c files in the current directory into object files
+gcc -c *.c
+ar rcs liball.a *.o
+rm *.o
